65ec16496b738668e39dfd776e6d61392fd13b6b
rafamolino <molinorafael99@gmail.com>
2018-11-27 12:24:01 +0100

    Nuevos Activity

--- app/src/main/java/com/example/pc/practica4pmdm/MainActivity.java
+++ app/src/main/java/com/example/pc/practica4pmdm/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.pc.practica4pmdm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), NumerosPrimos.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }
